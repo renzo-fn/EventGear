d44cf7e6f0fd95db2ec2eb19ef98e1b7db746547
unknown <renzoyupanqui29@gmail.com>
2025-06-09 00:09:15 -0500

    editar el catalogo.html

--- jss/catalogo.js
+++ jss/catalogo.js
@@ -1,14 +1,46 @@
-  let total = 0;
+let total = 0;
+    
+        function aumentar(id) {
+            const input = document.getElementById(`cantidad-${id}`);
+            let cantidad = parseInt(input.value, 10);
+            cantidad++;
+            input.value = cantidad;
+        }
 
-    function addToCart(precio) {
-      total += precio;
-      document.getElementById('total').textContent = `Total acumulado: S/${total.toFixed(2)}`;
-    }
+        function disminuir(id) {
+            const input = document.getElementById(`cantidad-${id}`);
+            let cantidad = parseInt(input.value, 10);
+            if (cantidad > 0) {
+                cantidad--;
+            }
+            input.value = cantidad;
+        }
+
+    function addToCart(id, nombreProducto, precio) {
+            const input = document.getElementById(`cantidad-${id}`);
+            const cantidad = parseInt(input.value, 10);
+
+            if (cantidad > 0) {
+                total += cantidad * precio; // Calcular y acumular el precio total
+                actualizarTotal();
+                input.value = 0; // Reiniciar cantidad
+            } else {
+                alert("La cantidad debe ser mayor a 0");
+            }}
+
+    function actualizarTotal() {
+            const totalElemento = document.getElementById("total");
+            totalElemento.textContent = `Total: $${total}`;
+        }
 
     function finalizarCompra() {
       alert(`El total de su compra es: S/${total.toFixed(2)}`);
       document.getElementById('formulario').classList.add('active'); // Muestra el formulario
     }
+
+    function mostrarFormulario() {
+            document.getElementById(`formulario`).classList.add('active');
+        }
 
     function cerrarFormulario() {
             document.getElementById(`formulario`).classList.remove('active');
